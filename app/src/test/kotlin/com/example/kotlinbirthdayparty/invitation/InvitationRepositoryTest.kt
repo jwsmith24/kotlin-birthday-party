7d73074d529dad84d9f8d7435debd538be13c4ad
Constantin Lodzeischi <constantin.lodzeischi.mil@army.mil>
2026-04-30 14:54:41 -0500

    fixed repo test

--- app/src/test/kotlin/com/example/kotlinbirthdayparty/invitation/InvitationRepositoryTest.kt
+++ app/src/test/kotlin/com/example/kotlinbirthdayparty/invitation/InvitationRepositoryTest.kt
@@ -12,28 +12,28 @@ class InvitationRepositoryTest {
     @Before
     fun setup() {
         invitationRepo = InvitationRepository()
-        testInvitation = Invitation(name = "Curt")
+        testInvitation = Invitation(name = "Yasmeen")
     }
 
     @Test
     fun whenUpsertCalledWithNewInvitation_thenAddToList() {
         invitationRepo.upsert(testInvitation)
 
-        assertEquals(1, invitationRepo.sentInvitations.value.size)
-        assertEquals("Curt", invitationRepo.sentInvitations.value[0].name)
+        assertEquals(4, invitationRepo.sentInvitations.value.size)
+        assertEquals("Yasmeen", invitationRepo.sentInvitations.value[3].name)
     }
 
     @Test
     fun whenUpsertCalledWithExistingInvitation_thenEntryUpdated() {
         invitationRepo.upsert(testInvitation)
-        assertEquals(1, invitationRepo.sentInvitations.value.size)
+        assertEquals(4, invitationRepo.sentInvitations.value.size)
         assertEquals(RsvpStatus.Pending, invitationRepo.sentInvitations.value.first().rsvpStatus)
 
         val targetInvitation = invitationRepo.sentInvitations.value.first()
         val updatedInvitation = targetInvitation.copy(rsvpStatus = RsvpStatus.Accepted)
         invitationRepo.upsert(updatedInvitation)
 
-        assertEquals(1, invitationRepo.sentInvitations.value.size)
+        assertEquals(4, invitationRepo.sentInvitations.value.size)
         assertEquals(RsvpStatus.Accepted, invitationRepo.sentInvitations.value.first().rsvpStatus)
     }
 
@@ -41,20 +41,20 @@ class InvitationRepositoryTest {
     fun whenRemoveCalledWithExistingId_thenEntryRemoved() {
         val targetId = 0
         invitationRepo.upsert(testInvitation)
-        assertEquals(1, invitationRepo.sentInvitations.value.size)
+        assertEquals(4, invitationRepo.sentInvitations.value.size)
 
         invitationRepo.remove(targetId)
 
-        assertEquals(0, invitationRepo.sentInvitations.value.size)
+        assertEquals(3, invitationRepo.sentInvitations.value.size)
     }
 
     @Test
     fun whenRemoveCalledWithNonExistingId_thenListUnchanged() {
         val targetId = 99
         invitationRepo.upsert(testInvitation)
-        assertEquals(1, invitationRepo.sentInvitations.value.size)
+        assertEquals(4, invitationRepo.sentInvitations.value.size)
 
         invitationRepo.remove(targetId)
-        assertEquals(1, invitationRepo.sentInvitations.value.size)
+        assertEquals(4, invitationRepo.sentInvitations.value.size)
     }
 }
